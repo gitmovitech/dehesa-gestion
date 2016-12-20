bf7ef689941cec1541888af1689bc91bfaae8ea8
Victor <victorandresv@gmail.com>
2016-12-20 00:21:00 -0300

    autoprocesar pac pat

---
+++ closepacpat.js
@@ -0,0 +1,61 @@
+var mongo = require('./mongodb');
+var ObjectID = require('mongodb').ObjectID;
+var month = 4;
+var valoruf = 25924.05;
+var pagos = [];
+setTimeout(function(){
+  mongo.getCollection('pagos', function(data){
+    for(var x in data){
+      if(data[x].month == month){
+        pagos[pagos.length] = data[x];
+      }
+    }
+    mongo.getCollection('asociados', function(data){
+      getAsociados(data, 0, function(){
+        console.log('Registros modificados');
+      });
+    });
+  });
+}, 3000);
+var getAsociados = function(data, index, cb){
+  if(data[index]){
+    if(data[index].formas_pago == 'PAT' || data[index].formas_pago == 'PAC'){
+      for(var v in pagos){
+        if(pagos[v].id == data[index].id){
+          if(pagos[v].debe != 0 && pagos[v].debe != '-'){
+            var estado;
+            if(data[index].formas_pago == 'PAT'){
+              estado = 'Aprobada';
+            } else {
+              estado = 'Pagos Procesados';
+            }
+            mongo.editCollection('pagos',[{
+              name: '_id',
+              value: ObjectID(pagos[v]._id)
+            }, {
+              name: 'pagado',
+              value: pagos[v].tarifa * valoruf
+            }, {
+              name: 'debe',
+              value: "0"
+            }, {
+              name: 'type',
+              value: estado
+            }], function(){
+              delete estado;
+              getAsociados(data, index + 1, cb);
+            });
+            break;
+          } else {
+            getAsociados(data, index + 1, cb);
+            break;
+          }
+        }
+      }
+    } else {
+      getAsociados(data, index + 1, cb);
+    }
+  } else {
+    cb();
+  }
+}
