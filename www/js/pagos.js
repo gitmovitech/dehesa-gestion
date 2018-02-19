ff705a78c2a40beacc5ad6e1162da1626798bf7a
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2018-02-19 10:12:08 -0300

    paginador ok

--- www/js/pagos.js
+++ www/js/pagos.js
@@ -1,5 +1,8 @@
 var years = [];
 var data;
+var listfrom = 0;
+var noactive = [];
+var current_page = 1;
 
 var GetUrlParams = function () {
     var url = location.href;
@@ -30,6 +33,11 @@ var modal_contacto = function (item) {
     }
 }
 
+var setPage = function (page) {
+    current_page = page;
+    listar();
+}
+
 
 var exportarPAT = function () {
     location.href = '/pagos/banco/pat/' + $('#filtro_ano').val() + '/' + $('#filtro_mes').val();
@@ -39,13 +47,69 @@ var exportarPAC = function () {
     location.href = '/pagos/banco/pac/' + $('#filtro_ano').val() + '/' + $('#filtro_mes').val();
 }
 
+var construirPaginador = function () {
+    var paginas = Math.ceil(data.length / $('#limitPerPage').val());
+    $('.paginador-foot').html('');
+    for (var n = 1; n <= paginas-1; n++) {
+        if (current_page == n) {
+            $('.paginador-foot').append('<a class="btn btn-xs btn-success active" href="javascript:setPage(' + n + ')">' + n + '</a> ');
+        } else {
+            $('.paginador-foot').append('<a class="btn btn-xs btn-success" href="javascript:setPage(' + n + ')">' + n + '</a> ');
+        }
+    }
+}
+
+var listar = function () {
+    $.getJSON('/api/carga/cobros', {
+        year: $('#filtro_ano').val(),
+        month: $('#filtro_mes').val()
+    }, function (response) {
+        $('#cobro_tbody, #costos_tbody').html('');
+        if (response.ok == 1) {
+            data = response.data;
+
+            construirPaginador();
+
+            var htmldata = "";
+            var contador = 0;
+            for (var i in response.data) {
+                if (current_page == 1) {
+                    listfrom = 0;
+                } else {
+                    listfrom = ($('#limitPerPage').val() * current_page) + 1;
+                }
+                if (i >= listfrom && contador <= $('#limitPerPage').val()) {
+                    contador++;
+                    $('#cobro_template .activo-checkbox').attr('id', 'active_' + response.data[i].id);
+                    $('#cobro_template .activo-checkbox').attr('checked', 'checked');
+                    $('#cobro_template .activo-checkbox').val(response.data[i].id)
+                    $('#cobro_template .asociado-id').html(response.data[i].id);
+                    $('#cobro_template .dias').html(response.data[i].dias);
+                    $('#cobro_template .tarifa').html('$' + response.data[i].tarifa.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
+                    $('#cobro_template .excedentes').html('$' + response.data[i].excedentes.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
+                    $('#cobro_template .ajuste').html('$' + response.data[i].ajuste_contable.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
+                    $('#cobro_template .deuda').html('$' + response.data[i].debe.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
+                    $('#cobro_template .btn-modal-contact').attr('onclick', 'javascript:modal_contacto(' + response.data[i].id + ')');
+                    htmldata += '<tr class="asociado-list template-clean" id="row_' + response.data[i].id + '">' + $('#cobro_template').html() + '</tr>';
+                }
+            }
+            $('#cobro_tbody, #costos_tbody').append(htmldata);
+        }
+        if (noactive.length > 0) {
+            noactive.forEach(function (item) {
+                $('#active_' + item).attr('checked', false);
+                $('#row_' + item).css('background-color', 'rgba(255,0,0,0.1)');
+            });
+        }
+    });
+}
+
 jQuery(function ($) {
 
     var params = GetUrlParams();
     var noexiste;
     var year = false;
     var month = false;
-    var noactive = [];
 
     if (params) {
         try {
@@ -71,6 +135,11 @@ jQuery(function ($) {
         $('#filtro_ano').append('<option value="' + n + '">' + n + '</option>');
     }
 
+    $('#filtrarLimites').click(function () {
+        current_page = 1;
+        listar();
+    });
+
     $('#filtrarButton').click(function () {
         $('.template-clean').remove();
         if ($('#filtro_ano').val() == '' || $('#filtro_mes').val() == '') {
@@ -82,41 +151,7 @@ jQuery(function ($) {
                 $('#contenido_pagos,#title').show();
                 $('#title h3').html('Cobros de ' + $('#filtro_mes').val());
 
-                year = $('#filtro_ano').val();
-
-                month = $('#filtro_mes').val();
-
-
-                $.getJSON('/api/carga/cobros', {
-                    year: year,
-                    month: month
-                }, function (response) {
-                    if (response.ok == 1) {
-                        data = response.data;
-                        var htmldata = "";
-                        for (var i in response.data) {
-                            //console.log(response.data[i]);
-                            $('#cobro_template .activo-checkbox').attr('id', 'active_' + response.data[i].id);
-                            $('#cobro_template .activo-checkbox').attr('checked', 'checked');
-                            $('#cobro_template .activo-checkbox').val(response.data[i].id)
-                            $('#cobro_template .asociado-id').html(response.data[i].id);
-                            $('#cobro_template .dias').html(response.data[i].dias);
-                            $('#cobro_template .tarifa').html('$' + response.data[i].tarifa.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
-                            $('#cobro_template .excedentes').html('$' + response.data[i].excedentes.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
-                            $('#cobro_template .ajuste').html('$' + response.data[i].ajuste_contable.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
-                            $('#cobro_template .deuda').html('$' + response.data[i].debe.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
-                            $('#cobro_template .btn-modal-contact').attr('onclick', 'javascript:modal_contacto(' + response.data[i].id + ')');
-                            htmldata += '<tr class="asociado-list template-clean" id="row_' + response.data[i].id + '">' + $('#cobro_template').html() + '</tr>';
-                        }
-                        $('#cobro_tbody, #costos_tbody').append(htmldata);
-                    }
-                    if (noactive.length > 0) {
-                        noactive.forEach(function (item) {
-                            $('#active_' + item).attr('checked', false);
-                            $('#row_' + item).css('background-color', 'rgba(255,0,0,0.1)');
-                        });
-                    }
-                });
+                listar();
 
             });
         }
