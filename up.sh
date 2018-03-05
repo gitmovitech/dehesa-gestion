a498757662f31e4d1fbde018bdeba31d60e38c9c
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2018-03-05 15:08:59 -0300

    @mensaje

--- up.sh
+++ up.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo 'Mensaje:'
+read mensaje
 git add .
-git commit -m 'upload'
+git commit -m @mensaje
 git push origin master
 
 
@@ -14,12 +16,3 @@ git push origin master
  # expect eof
 #DONE
 
-expect <<- DONE
-  set timeout -1
-  spawn ssh root@gestion.jvdehesa.cl -p 22222 "cd /var/www/html/gestion/;sh down.sh;";
-  match_max 100000
-  expect "*?assword:*"
-  send -- "e8SVfS1ilYR1Zo93d0\r"
-  send -- "\r"
-  expect eof
-DONE
